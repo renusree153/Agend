5c98d6ece3472f5716c0dc26ab3bbab518c0469e
rrenusree <renusree@uw.edu>
2023-08-12 01:41:48 -0700

    in progress settings file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+import Settings from './Settings';
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element = {<SignIn/>} />
       <Route path="/signUp" element = {<SignUp/>} />
+      <Route path="/settings" element = {<Settings/>}/>
     </Routes>
   );
 }
---
+++ src/Settings.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './Settings.css';
+
+const Settings = () => {
+    return (
+        <div className="container">
+            <div className="blueSide">
+                <div class ="circle">
+                </div>
+                <h3>Jane Doe</h3>
+                <h4>Janedoe@gmail.com</h4>
+                <div className = "menu">
+                    <h3> Conversations</h3>
+                    <h3>Personal Calendar</h3>
+                    <h3>Teams</h3>
+                    <h3>Find Friends</h3>
+                    <h3>Settings</h3>
+                </div>
+            </div>
+            <div className="whiteSide">
+                <h1>Settings</h1>
+                <h1>HIIIII</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Settings;

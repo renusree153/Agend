acbd5e68d477ff468b567800202c492feb01b1a4
rrenusree <renusree@uw.edu>
2023-09-06 11:53:22 -0700

    styling isnt working

--- src/TeamCalendar.js
+++ src/TeamCalendar.js
@@ -10,10 +10,25 @@ const ReactCalendar = () => {
         setDate(date)
     };
 
-
     return (
-        <div>
-            <Calendar onChange={onChange} value={date} />
+        <div className ="container2">
+            <div className="blueSide">
+            <div class ="circle">
+            </div>
+            <h3>Jane Doe</h3>
+            <h4>Janedoe@gmail.com</h4>
+            <div className = "menu">
+                <h3> Conversations</h3>
+                <h3>Personal Calendar</h3>
+                <h3>Teams</h3>
+                <h3>Find Friends</h3>
+                <h3>Settings</h3>
+                <h3>Notifications</h3>
+            </div>
+            <div className = "cal">
+                <Calendar onChange={onChange} value={date} />
+            </div>
+        </div>
         </div>
     )
 }

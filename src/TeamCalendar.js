3f07a3b038dd803e3a38ebf7053b5b7088f770bc
rrenusree <renusree@uw.edu>
2023-09-06 01:10:57 -0700

    cal takes full page

--- src/TeamCalendar.js
+++ src/TeamCalendar.js
@@ -1,42 +1,21 @@
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import Calendar from 'react-calendar';
+import './TeamCalendar.css';
 
-export function DemoApp() {
-  const [events, setEvents] = useState([
-    { title: 'Meeting', start: new Date() },
-  ]);
+const ReactCalendar = () => {
+    const [date, setDate] = useState(new Date());
+    const onChange = date => {
+        setDate(date)
+    };
 
-  const handleDayClick = (arg) => {
-    const title = prompt('Enter event title:');
-    if (title) {
-      const newEvent = { title, start: arg.date, allDay: true };
-      setEvents([...events, newEvent]);
-    }
-  };
 
-  function renderEventContent(eventInfo) {
     return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </>
-    );
-  }
-
-  return (
-    <div>
-      <h1>Demo App</h1>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView='dayGridMonth'
-        weekends={false}
-        events={events}
-        eventContent={renderEventContent}
-        dateClick={handleDayClick} // Attach the day click handler
-      />
-    </div>
-  );
+        <div>
+            <Calendar onChange={onChange} value={date} />
+        </div>
+    )
 }
 
-export default DemoApp;
+export default ReactCalendar;

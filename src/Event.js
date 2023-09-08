989036a452b59948345eeaacd78f158e3163a547
rrenusree <renusree@uw.edu>
2023-09-08 00:05:01 -0700

    building accordion

--- src/Event.js
+++ src/Event.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {useState} from 'react';
 import "./Event.css";
 
 const NewEvent = () => {
+    const [selected, setSelected] = useState(null)
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null);
+        }
+        setSelected(i);
+    }
     return (
         <div className = "eventCard">
             <h2 id="name"> Anika's Birthday </h2>
             <h2 id="time"> 9pm - 10pm </h2>
+            <h2 id="plus"> + </h2>
         </div>
     )
 }
